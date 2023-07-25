0ae601125b7ae7c5f12d3fe33cf19f00bd2fafa5
conquest2023 <dlqns928@naver.com>
2023-07-25 19:54:53 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -4,9 +4,9 @@ const app=express();
 
 const PORT=3000;
 //라우팅
-const home=require("./routes/home");
+const home=require("./src/routes/home");
 //앱 세팅
-app.set("views","./views");
+app.set("views","./src/views");
 app.set("view engine","ejs");
 app.use(express.static(`${__dirname}/src/public`));
 
