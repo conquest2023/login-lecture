14d86118e7e878bfab87f181147feb90b525482d
conquest2023 <dlqns928@naver.com>
2023-07-26 13:57:58 +0900

    유저 모델 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -22,13 +22,13 @@ const id =document.querySelector("#id"),
       })
       .then((res)=>res.json())
       .then((res)=>{
-        if(res.success){
+        if(res.success) {
             location.href="/";
         }else {
             alert(res.msg);
         }
       })
       .catch((err)=>{
-        console.err(new Error("로그인 중 에러 발생"));
+        console.err("로그인 중 에러 발생");
       });
     }
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,8 +1,7 @@
 "use strict";
-const users={
-    id:["hi","나개발","김팀장"],
-    psword:["1234","1234","123456"],
-};
+
+const User = require("../../models/User");
+const UserStorage=require("../../models/UserStorage")
 const output={
      home:(req,res) =>{
         res.render("home/index");
@@ -14,21 +13,11 @@ const output={
 };
 const process={
     login: (req,res)=>{
-        const id=req.body.id,
-        psword=req.body.psword;
-
-        if(users.id.includes(id)){
-            const idx=users.id.indexOf(id);
-            if(users.psword[idx]===psword){
-                return res.json({
-                    success:true,
-                });
-            }
-        }
-        return res.json({
-            success:false,
-            msg:"로그인에 실패하였습니다",
-        });
+        const user=new User(req.body);
+        const response=user.login();
+        return res.json(response);
+      
+       
     },
 };
 
@@ -36,3 +25,5 @@ module.exports={
     output,
     process,
 };
+
+
